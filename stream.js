9c7c7c02dab55549e4f1372d66f50dc045ec3309
Anand Thakker <vestibule@anandthakker.net>
2014-12-21 20:39:42 +0100

    don't instance postcss each chunk

--- stream.js
+++ stream.js
@@ -10,17 +10,24 @@ module.exports = stream;
 
 function stream(browsers) {
   var inp = rules();
+
+  var processor = postcss(doiuse({
+    browserSelection: browsers,
+    onUnsupportedFeatureUse: pushUsage
+  }));
+
   var out = through.obj(function(rule, enc, next) {
     try {
-      postcss(doiuse({
-        browserSelection: browsers,
-        onUnsupportedFeatureUse: this.push.bind(this)
-      })).process(rule.content);
+      processor.process(rule.content);
     }
     catch(e) {
       next(e);
     }
   });
+  
+  function pushUsage(usage) {
+    out.push(usage);
+  }
 
   inp.pipe(out);
   return duplexer(inp, out);
