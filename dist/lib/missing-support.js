ec0150312e0c43a9df86644ecfd50ea642fab8ec
Anand Thakker <vestibule@anandthakker.net>
2015-05-30 15:01:48 -0400

    decaffeinate missing-support

--- dist/lib/missing-support.js
+++ dist/lib/missing-support.js
@@ -1,72 +1,71 @@
-var BrowserSelection, features, filterStats, formatBrowserName, fs, missing, _;
+'use strict';
 
-features = require('../data/features');
+var features = require('../data/features');
+var BrowserSelection = require('./browsers');
+var _ = require('lodash');
+var fs = require('fs');
+var formatBrowserName = require('./util').formatBrowserName;
 
-BrowserSelection = require('./browsers');
-
-_ = require('lodash');
-
-fs = require('fs');
-
-formatBrowserName = require('./util').formatBrowserName;
-
-filterStats = function(browsers, stats) {
-  return _.transform(stats, function(resultStats, versionData, browser) {
-    var versionsWithoutSupport;
-    versionsWithoutSupport = _.transform(versionData, function(result, support, ver) {
-      var selected;
-      selected = browsers.test(browser, ver);
-      if ((selected != null) && (!/(^|\s)y($|\s)/.test(support))) {
-        return result[selected[1]] = support;
+function filterStats(browsers, stats) {
+  return _.transform(stats, function (resultStats, versionData, browser) {
+    // filter only versions of selected browsers that don't support this
+    // feature (i.e., don't have 'y' in their stats)
+    var versWithoutSupport = _.transform(versionData, function (result, support, ver) {
+      var selected = browsers.test(browser, ver);
+      if (selected && !/(^|\s)y($|\s)/.test(support)) {
+        result[selected[1]] = support;
       }
     });
-    if (_.keys(versionsWithoutSupport).length !== 0) {
-      return resultStats[browser] = versionsWithoutSupport;
+    // filter out browsers for which there are *no* (selected) versions lacking
+    // support.
+    if (_.keys(versWithoutSupport).length !== 0) {
+      resultStats[browser] = versWithoutSupport;
     }
   });
-};
-
-
-/*
-Get data on CSS features not supported by the given autoprefixer-like browser
-selection.
-
-Returns:
-```
-{
-  'feature-name': {
-    title: 'Title of feature'
-    missing: "IE (8), Chrome (31)"
-    missingData: {
-      // map of browser -> version -> (lack of)support code
-      ie: { '8': 'n' },
-      chrome: { '31': 'n' }
-    }
-    caniuseData: {
-      // caniuse-db json data for this feature
-    }
-  },
-  'feature-name-2': {} // etc.
 }
-```
 
-`feature-name` is a caniuse-db slug.
+/**
+ * Get data on CSS features not supported by the given autoprefixer-like
+ * browser selection.
+ *
+ * @returns `{browsers, features}`, where `features` is an array of:
+ * ```
+ * {
+ *   'feature-name': {
+ *     title: 'Title of feature'
+ *     missing: "IE (8), Chrome (31)"
+ *     missingData: {
+ *       // map of browser -> version -> (lack of)support code
+ *       ie: { '8': 'n' },
+ *       chrome: { '31': 'n' }
+ *     }
+ *     caniuseData: {
+ *       // caniuse-db json data for this feature
+ *     }
+ *   },
+ *   'feature-name-2': {} // etc.
+ * }
+ * ```
+ *
+ * `feature-name` is a caniuse-db slug.
  */
+function missing(browserRequest) {
+  var browsers = new BrowserSelection(browserRequest);
 
-missing = function(browserRequest) {
-  var browsers, data, feature, featureData, json, missingData, result;
-  browsers = new BrowserSelection(browserRequest);
-  result = {};
-  for (feature in features) {
-    data = features[feature];
-    json = fs.readFileSync(require.resolve('caniuse-db/features-json/' + feature));
-    featureData = JSON.parse(json);
-    missingData = filterStats(browsers, featureData.stats);
-    missing = _.reduce(missingData, function(res, versions, browser) {
+  var result = {};
+
+  Object.keys(features).forEach(function (feature) {
+    var json = fs.readFileSync(require.resolve('caniuse-db/features-json/' + feature));
+    var featureData = JSON.parse(json);
+    var missingData = filterStats(browsers, featureData.stats);
+
+    // browsers missing support for this feature
+    var missing = _.reduce(missingData, function (res, versions, browser) {
       res.push(formatBrowserName(browser, _.keys(versions)));
       return res;
     }, []).join(', ');
-    if (missing.length !== 0) {
+
+    if (missing.length > 0) {
       result[feature] = {
         title: featureData.title,
         missing: missing,
@@ -74,11 +73,12 @@ missing = function(browserRequest) {
         caniuseData: featureData
       };
     }
-  }
+  });
+
   return {
     browsers: browsers.list(),
     features: result
   };
-};
+}
 
 module.exports = missing;
---
+++ src/lib/missing-support.js
@@ -0,0 +1,84 @@
+let features = require('../data/features')
+let BrowserSelection = require('./browsers')
+let _ = require('lodash')
+let fs = require('fs')
+let formatBrowserName = require('./util').formatBrowserName
+
+function filterStats (browsers, stats) {
+  return _.transform(stats, (resultStats, versionData, browser) => {
+    // filter only versions of selected browsers that don't support this
+    // feature (i.e., don't have 'y' in their stats)
+    let versWithoutSupport = _.transform(versionData, (result, support, ver) => {
+      let selected = browsers.test(browser, ver)
+      if (selected && (!/(^|\s)y($|\s)/.test(support))) {
+        result[selected[1]] = support
+      }
+    })
+    // filter out browsers for which there are *no* (selected) versions lacking
+    // support.
+    if (_.keys(versWithoutSupport).length !== 0) {
+      resultStats[browser] = versWithoutSupport
+    }
+  })
+}
+
+/**
+ * Get data on CSS features not supported by the given autoprefixer-like
+ * browser selection.
+ *
+ * @returns `{browsers, features}`, where `features` is an array of:
+ * ```
+ * {
+ *   'feature-name': {
+ *     title: 'Title of feature'
+ *     missing: "IE (8), Chrome (31)"
+ *     missingData: {
+ *       // map of browser -> version -> (lack of)support code
+ *       ie: { '8': 'n' },
+ *       chrome: { '31': 'n' }
+ *     }
+ *     caniuseData: {
+ *       // caniuse-db json data for this feature
+ *     }
+ *   },
+ *   'feature-name-2': {} // etc.
+ * }
+ * ```
+ *
+ * `feature-name` is a caniuse-db slug.
+ */
+function missing (browserRequest) {
+  let browsers = new BrowserSelection(browserRequest)
+
+  let result = {}
+
+  Object.keys(features).forEach((feature) => {
+    let json = fs.readFileSync(require.resolve('caniuse-db/features-json/'
+      + feature))
+    let featureData = JSON.parse(json)
+    let missingData = filterStats(browsers, featureData.stats)
+
+    // browsers missing support for this feature
+    let missing = _.reduce(missingData, (res, versions, browser) => {
+      res.push(formatBrowserName(browser, _.keys(versions)))
+      return res
+    }, [])
+    .join(', ')
+
+    if (missing.length > 0) {
+      result[feature] = {
+        title: featureData.title,
+        missing,
+        missingData,
+        caniuseData: featureData
+      }
+    }
+  })
+
+  return {
+    browsers: browsers.list(),
+    features: result
+  }
+}
+
+module.exports = missing
