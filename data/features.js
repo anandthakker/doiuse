dc42f1878f09b7cbbf18b41b777625402b1ab1a8
Marek Dědič <developer@dedic.eu>
2020-11-10 02:02:22 +0100

    matchOutsideOfBrackets as a function, not a complicated regex

--- data/features.js
+++ data/features.js
@@ -1,16 +1,15 @@
 var list = require('postcss/lib/list')
 var pats = {
   attrcc: '[^\\~|^$*\\]]*',
-  brackets: /(\[[^\]]*\]|\([^\)]*\))/.source,
-  nobrackets: /[^\[\]\(\)]/.source
+  brackets: /\[[^\]]*\]|\([^\)]*\)/g
 }
 function matchOutsideOfBrackets(pat) {
   if (!(pat instanceof RegExp)) {
     throw new TypeError('matchOutsideOfBrackets expects a RegExp')
   }
-  return new RegExp(
-    '^(' + pats.brackets + '?' + pats.nobrackets + '*)*?' + pat.source
-  )
+  return function(str) {
+	return pat.test(str.replace(pats.brackets, ''))
+  }
 }
 
 module.exports = {
