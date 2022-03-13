0df83eee065652b4ec86459176635505df303025
Carlos Lopez Jr <clshortfuse@gmail.com>
2022-03-13 18:52:51 -0400

    feat(css-grid): add missing properties

--- data/features/css-grid.js
+++ data/features/css-grid.js
@@ -1,3 +1,19 @@
+/** @see https://drafts.csswg.org/css-grid/ */
+
 export default {
-  'display': ['grid', 'inline-grid'],
+  'display': ['grid', 'inline-grid', '-ms-grid'],
+  'grid': true,
+  'grid-area': true,
+  'grid-auto-columns': true,
+  'grid-auto-rows': true,
+  'grid-column': true,
+  'grid-columns-end': true,
+  'grid-columns-start': true,
+  'grid-row': true,
+  'grid-row-end': true,
+  'grid-row-start': true,
+  'grid-template': true,
+  'grid-template-areas': true,
+  'grid-template-columns': true,
+  'grid-template-rows': true,
 };
