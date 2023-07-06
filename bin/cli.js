28423428a98dd381804098f3178b878d9c4ee1d9
Carlos Lopez Jr <clshortfuse@gmail.com>
2023-07-05 21:13:16 -0400

    refactor(cli): use typed yargs

--- bin/cli.js
+++ bin/cli.js
@@ -8,57 +8,59 @@ import { PassThrough } from 'node:stream';
 
 import browserslist from 'browserslist';
 import ldjson from 'ldjson-stream';
-import yargsImport from 'yargs';
+import yargs from 'yargs';
 
 import DoIUse from '../lib/DoIUse.js';
 import CssUsageDuplex from '../lib/stream/CssUsageDuplex.js';
 import { formatBrowserName } from '../utils/util.js';
 
 const FILE_NOT_FOUND = 'ENOENT';
-const yargs = yargsImport(process.argv.slice(2));
-
-yargs
+const argv = await yargs(process.argv.slice(2))
   .usage('Lint your CSS for browser support.')
   .example('cat FILE | $0 -b "ios >= 6"', '')
   .example('$0 --browsers "ie >= 9, > 1%, last 3 versions" [FILE] [FILE] ...', '')
   .example('$0 -b "ie >= 8" -b "> 1%" -b "last 3 versions" [FILE] [FILE] ...', '')
-  .options('b', {
-    alias: 'browsers',
-    description: 'Autoprefixer-like browser criteria.',
-    default: null,
-  })
-  .string('b')
-  .options('i', {
-    alias: 'ignore',
-    description: 'List of features to ignore.',
-    default: '',
-  })
-  .string('i')
-  .options('l', {
-    alias: 'list-only',
-    description: 'Just show the browsers and features that would be tested by'
+  .options({
+    browsers: {
+      type: 'string',
+      alias: 'b',
+      description: 'Autoprefixer-like browser criteria.',
+      default: /** @type {string} */ (null),
+      string: true,
+    },
+    ignore: {
+      type: 'string',
+      alias: 'i',
+      description: 'List of features to ignore.',
+      default: '',
+      string: true,
+    },
+    l: {
+      type: 'boolean',
+      alias: 'list-only',
+      description: 'Just show the browsers and features that would be tested by'
       + 'the specified browser criteria, without actually processing any CSS.',
-  })
-  .options('c', {
-    alias: 'config',
-    description: 'Provide options through config file',
-  })
-  .string('verbose')
-  .options('v', {
-    alias: 'verbose',
-    description: 'Verbose output. Multiple levels available.',
+    },
+    config: {
+      type: 'string',
+      alias: 'c',
+      description: 'Provide options through config file',
+    },
+    verbose: {
+      type: 'number',
+      alias: 'v',
+      description: 'Verbose output. Multiple levels available.',
+    },
+    json: {
+      type: 'boolean',
+      alias: 'j',
+      description: 'Output JSON instead of string linter-like messages.',
+    },
   })
   .count('verbose')
-  .options('j', {
-    alias: 'json',
-    description: 'Output JSON instead of string linter-like messages.',
-  })
-  .boolean('j')
   .help('h', 'Show help message.')
-  .alias('h', 'help');
-
-/** @type {Record<string,any>} */
-const { argv } = yargs;
+  .alias('h', 'help')
+  .argv;
 
 // Config file reading
 if (argv.config) {
@@ -80,19 +82,14 @@ if (argv.config) {
   }
 }
 
-if (argv.browsers) {
-  argv.browsers = argv.browsers.split(',')
-    .map((s) => s.trim());
-}
-argv.ignore = argv.ignore.split(',').map((s) => s.trim());
+const browsers = argv.browsers ? argv.browsers.split(',')
+  .map((/** @type {string} */ s) => s.trim()) : null;
+
+const ignore = argv.ignore.split(',').map((s) => s.trim());
 
 // Informational output
-if (argv.l) {
-  argv.verbose += 1;
-  argv.v = argv.verbose;
-}
-if (argv.verbose >= 1) {
-  const browsers = browserslist(argv.browsers)
+if (argv.l || argv.verbose >= 1) {
+  const formattedBrowsers = browserslist(browsers)
     .map((b) => {
       const [name, version] = b.split(' ');
       /** @type {[string, number]} */
@@ -102,11 +99,11 @@ if (argv.verbose >= 1) {
     .sort((a, b) => b[1] - a[1])
     .map((b) => b.join(' '))
     .join(', ');
-  process.stdout.write(`[doiuse] Browsers: ${browsers}\n`);
+  process.stdout.write(`[doiuse] Browsers: ${formattedBrowsers}\n`);
 }
 
 if (argv.verbose >= 2) {
-  const { features } = new DoIUse(argv.browsers).info();
+  const { features } = new DoIUse({ browsers }).info();
   process.stdout.write('[doiuse] Unsupported features:\n');
   for (const feature of Object.values(features)) {
     process.stdout.write(`${feature.caniuseData.title}\n`);
@@ -147,13 +144,17 @@ outStream.pipe(process.stdout);
  * @param {string} [file]
  */
 function processStream(stream, file) {
-  stream.pipe(new CssUsageDuplex({ browsers: argv.browsers, ignore: argv.ignore }, file))
+  stream.pipe(new CssUsageDuplex({
+    browsers: argv.browsers,
+    // @ts-expect-error Skip cast
+    ignore,
+  }, file))
     .on('error', (error) => { console.error(error); })
     .pipe(outStream);
 }
 if (argv._.length > 0) {
   for (const file of argv._) {
-    processStream(fs.createReadStream(file), file);
+    processStream(fs.createReadStream(file.toString()), file.toString());
   }
 } else {
   processStream(process.stdin);
