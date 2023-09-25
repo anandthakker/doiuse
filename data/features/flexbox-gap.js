5199d866ed88d86b2c833078ee62264bb9604226
Дмитрий Жук <d.zhuk@corp.mail.ru>
2023-09-25 15:20:54 +0300

    fix fail test

--- data/features/flexbox-gap.js
+++ data/features/flexbox-gap.js
@@ -8,11 +8,21 @@
  * @type {import('../features').Feature}
  */
 export default (rule) => {
-  if (/display:\s(inline-)?flex/.test(rule.toString())
-    && rule.toString().includes('gap:')
-  ) {
-    return true;
+  if (rule.type !== 'rule') {
+    return false;
   }
 
-  return false;
+  let isFlexbox = false;
+  let isGap = false;
+
+  return rule.some((decl) => {
+    if (decl.type !== 'decl') {
+      return false;
+    }
+
+    isFlexbox ||= decl.prop === 'display' && (decl.value === 'flex' || decl.value === 'inline-flex');
+    isGap ||= decl.prop === 'gap';
+
+    return isFlexbox || isGap;
+  });
 };
